425b121e9eaf19a85592b591d0c34d5c9b7372f7
Sakimonk <y.e.badi@se11.qmul.ac.uk>
2017-03-06 11:03:23 +0000

    added Rcore

---
+++ Rcore.R
@@ -0,0 +1,27 @@
+##### Analysis of datasets 2017 Yusef Badi ###########
+
+rm (list =ls())  # clear the R workspace
+
+######### loading the required packages ###############
+
+
+source("https://bioconductor.org/biocLite.R")
+
+biocLite("ggplot2")
+biocLite("limma")
+biocLite("edgeR")
+biocLite("dendextend")
+biocLite("calibrate")
+
+###########   setting up libraries  #########
+
+library(ggplot2)
+
+# we will use ggplot2 to generate graphs, then use 
+# plotly javascript to make interactive graphs
+
+library(limma)
+library(edgeR)
+library(dendextend)
+library(calibrate)
+
