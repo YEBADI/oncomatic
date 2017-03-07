65f6073a7a6c84b837e21218dc07fc3cbb63e0f9
Sakimonk <y.e.badi@se11.qmul.ac.uk>
2017-03-07 10:16:58 +0000

    added new text

--- TCGA/TCGA_Rscript.R
+++ TCGA/TCGA_Rscript.R
@@ -12,8 +12,39 @@ library(TCGAbiolinks)
 library(dplyr)
 library(DT)
 
+###################################
+#### ignore this section
+#### datatable needs to run on apocrita
+#### can't run on PC
+#
+##### downloading XML-indexed data & setting up datatable#
+# clinical <- GDCquery_clinic(project = "TCGA-BRCA", type = "clinical")
+# # ^this works
+# datatable(clinical, filter = 'top', options = list(scrollX = TRUE, keys = TRUE, pageLength = 5), rownames = FALSE)
+# # ^ this says can't run on laptop as data too big 
+##################################
 
-# downloading XML-indexed data
-clinical <- GDCquery_clinic(project = "TCGA-LUAD", type = "clinical")
 
-datatable(clinical, filter = 'top', options = list(scrollX = TRUE, keys = TRUE, pageLength = 5), rownames = FALSE)
+# We can easily search GDC data for data to download
+# using the GDCquery function.
+# Using filters akin to the filters in the TCGA portal
+
+# we won't need to look at methylations
+# also primary concern is breast and pancreatic tissues
+
+# direct query
+query <- GDCquery(project = "TCGA-BRCA",
+                  data.category = "Transcriptome Profiling",
+                  data.type = "Gene Expression Quantification", 
+                  workflow.type = "HTSeq - Counts")
+
+
+query.mirna <- GDCquery(project = "TCGA-BRCA", 
+                        data.category = "Transcriptome Profiling", 
+                        data.type = "miRNA Expression Quantification",
+                        sample.type = c("Primary solid Tumor","Solid Tissue Normal"))
+
+query.mirna <- GDCquery(project = "TCGA-ACC", 
+                        data.category = "Transcriptome Profiling", 
+                        data.type = "miRNA Expression Quantification",
+                        sample.type = c("Primary solid Tumor","Solid Tissue Normal"))
