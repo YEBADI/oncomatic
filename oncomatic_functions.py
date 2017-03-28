261d5553070099d7ef3b948b4e0253ec4196ffb6
Sakimonk <y.e.badi@se11.qmul.ac.uk>
2017-03-28 11:49:43 +0100

    python app functions script

---
+++ oncomatic_functions.py
@@ -0,0 +1,9 @@
+# tumor type = "BRCA" / "PAAD"
+# show subtypes = yes / no
+# pipeline = "mutect2" / "muse" / "varscan2" / "somaticsniper"
+# number of top genes = 20
+# gene of interest = show all / "ATM-BRCA1-P53"
+import sys
+import os.path
+
+def runinputcheck():
